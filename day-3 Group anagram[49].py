1e57210f17509f77ae59bf5fcd449775f7bfafa6
Akshaya <zyaakshu@gmail.com>
2026-02-13 20:14:14 +0530

    Create day-3 Group anagram[49].py

---
+++ day-3 Group anagram[49].py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        group={}
+        for word in strs:
+            key=''.join(sorted(word))
+            if key not in group:
+                group[key]=[]
+            group[key].append(word)
+        return group.values()
