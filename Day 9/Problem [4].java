602b73678203fdfb1f4551f7c6137b1100b9ee2f
Akshaya <zyaakshu@gmail.com>
2026-02-25 20:27:52 +0530

    Create Problem [4].java

---
+++ Day 9/Problem [4].java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> last = new HashMap<>();
+        int start = 0, maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (last.containsKey(c) && last.get(c) >= start) {
+                start = last.get(c) + 1;
+            }
+            last.put(c, i);
+            maxLen = Math.max(maxLen, i - start + 1);
+        }
+        return maxLen;
+    }
+}
