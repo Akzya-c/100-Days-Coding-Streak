5ff0da196d8ef3467267f0079a7328df21d552a4
Akshaya <zyaakshu@gmail.com>
2026-03-01 20:34:20 +0530

    Update Find Smallest Letter [744].java

--- Day 8/Find Smallest Letter [744].java
+++ Day 8/Find Smallest Letter [744].java
@@ -1,14 +1,19 @@
 class Solution {
-    public char nextGreatestLetter(char[] letters, char target) {
+    public char nextGreatestLetter(char[] letters, char target) 
+    {
         int s = 0;
         int e = letters.length - 1;
 
-        while (s <= e) {
+        while (s <= e) 
+        {
             int mid = s + (e - s) / 2;
 
-            if (letters[mid] <= target) {
+            if (letters[mid] <= target) 
+            {
                 s = mid + 1;
-            } else {
+            }
+            else 
+            {
                 e = mid - 1;
             }
         }
