0587e610ac898629f88760dab1f633312abc841f
Akshaya <zyaakshu@gmail.com>
2026-03-03 20:15:50 +0530

    Update day-2 solved Intersection of array [349].py

--- Day 2/day-2 solved Intersection of array [349].py
+++ Day 2/day-2 solved Intersection of array [349].py
@@ -2,7 +2,6 @@ class Solution(object):
     def intersection(self, nums1, nums2):
         set1 = set(nums1)
         result = set()
-        
         for num in nums2:
             if num in set1:
                 result.add(num)
