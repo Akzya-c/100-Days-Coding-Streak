52a8516c1e5d4e8bce4b038b310702e898f29504
Akshaya <zyaakshu@gmail.com>
2026-03-04 20:04:46 +0530

    Update Longest Palindromic Substring[5].py

--- Day 9/Longest Palindromic Substring[5].py
+++ Day 9/Longest Palindromic Substring[5].py
@@ -9,11 +9,10 @@ class Solution:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-            return left + 1, right - 1  # final valid bounds
-
+            return left + 1, right - 1  
         for i in range(len(s)):
-            l1, r1 = expand(i, i)       # odd length
-            l2, r2 = expand(i, i + 1)   # even length
+            l1, r1 = expand(i, i)       
+            l2, r2 = expand(i, i + 1)   
 
             if r1 - l1 > end - start:
                 start, end = l1, r1
